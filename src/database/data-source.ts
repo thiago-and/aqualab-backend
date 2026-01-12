8c9d20456439791c52f75f96af6825bc57e7eb4f
thiagobrazil <tandrade94@live.com>
2026-01-11 22:49:10 -0300

    fix data-source for prod js and dev ts

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,17 +1,24 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
 
+const isProd = process.env.NODE_ENV === "production";
+
 export const AppDataSource = new DataSource({
-    type: "mariadb",
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  type: "mariadb",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
-    entities: [__dirname + "/../entities/**/*.{ts,js}"],
-    migrations: [__dirname + "/migrations/**/*.{ts,js}"],
+  entities: isProd
+    ? ["build/entities/**/*.{js}"]
+    : ["src/entities/**/*.{ts}"],
 
-    synchronize: false,
-    logging: false,
+  migrations: isProd
+    ? ["build/database/migrations/**/*.{js}"]
+    : ["src/database/migrations/**/*.{ts}"],
+
+  synchronize: false,
+  logging: false,
 });

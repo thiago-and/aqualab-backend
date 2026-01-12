cfee5005283d0035e3495710214c87a313cebe34
thiagobrazil <tandrade94@live.com>
2026-01-11 23:49:02 -0300

    enable cors

--- src/server.ts
+++ src/server.ts
@@ -11,8 +11,19 @@ import { quizRoutes } from './routes/quizzes/quiz.routes'
 import { studentQuizRoutes } from './routes/quizzes/studentQuiz.routes';
 import "./config/env";
 import { quizAttemptRoutes } from './routes/quizzes/quizAttempt.routes';
+import cors from 'cors';
 
 const server = express();
+
+server.use(cors({
+    origin: [
+        "https://aqualab-frontend.vercel.app",
+        "http://localhost:3000"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: false
+}));
 
 server.use(express.json());
 
